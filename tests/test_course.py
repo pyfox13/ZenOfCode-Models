839e1f7ae17d9f879d5e389b8366fe254cd08762
pyfox <pythonawspractice@gmail.com>
2025-04-30 01:09:56 -0400

    test ruleset

--- tests/test_course.py
+++ tests/test_course.py
@@ -2,6 +2,6 @@ from models.course import Course
 
 
 def test_course_fields():
-    course = Course(name="Python 101", description="Intro course")
+    course = Course(name="Python 101", description="Intro course.")
     assert course.name == "Python 101"
-    assert course.description == "Intro course"
+    assert course.description == "Intro course."
